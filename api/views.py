0c2833aa07863555ca4ce5bbcf10d4bf1f816468
Yuvraj-Rathod <yr881709@gmail.com>
2023-05-24 16:53:26 +0530

    first commit

--- api/views.py
+++ api/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+#GET
 @api_view(['GET'])
 def hello_world(request):
     return Response({'msg':'hello world'})
 
-
+#POST 
 @api_view(['POST'])
 def hello_world(request):
     if request.method == 'POST':
